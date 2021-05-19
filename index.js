09d97f6220db7dbc91fb35a94bec237b2251a93a
alexandra9402kr <alexandra94.02kr@mail.ru>
2021-05-19 16:27:32 +0300

    фон

---
+++ index.js
@@ -0,0 +1,11 @@
+function backgroundColor() {
+    if (document.getElementById("color").value == "blue") {
+        document.getElementById("background-color").style.backgroundColor='blue';
+    }
+    if (document.getElementById("color").value == "yellow") {
+        document.getElementById("background-color").style.backgroundColor='yellow';
+    }
+    if (document.getElementById("color").value == "white") {
+        document.getElementById("background-color").style.backgroundColor='white';
+    }
+}
